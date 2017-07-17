47dbbbbc777cacc74ca380022ed32ac8223d816c
Jackie Johnston <jackie.johnston@pnmac.com>
2017-07-17 10:22:48 -0700

    feat: Pull books array from API

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
+
   state = {
     /**
      * TODO: Instead of using this state variable to keep track of which page
@@ -10,7 +11,15 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    showSearchPage: false,
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+      // console.log({ books })
+    })
   }
 
   render() {
@@ -26,51 +35,6 @@ class BooksApp extends React.Component {
       {
         title: "Read",
         category: "read"
-      }
-    ]
-
-    const books = [
-      {
-        bookTitle: "To Kill a Mockingbird",
-        bookAuthors: "Harper Lee",
-        bookCover: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
-        bookShelf: "currentlyReading"
-      },
-      {
-        bookTitle: "Ender's Game",
-        bookAuthors: "Orson Scott Card",
-        bookCover: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
-        bookShelf: "currentlyReading"
-      },
-      {
-        bookTitle: "1776",
-        bookAuthors: "David McCullough",
-        bookCover: "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
-        bookShelf: "wantToRead"
-      },
-      {
-        bookTitle: "Harry Potter and the Sorcerer's Stone",
-        bookAuthors: "J.K. Rowling",
-        bookCover: "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api",
-        bookShelf: "wantToRead"
-      },
-      {
-        bookTitle: "The Hobbit",
-        bookAuthors: "J.R.R. Tolkien",
-        bookCover: "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api",
-        bookShelf: "read"
-      },
-      {
-        bookTitle: "Oh, the Places You'll Go!",
-        bookAuthors: "Seuss",
-        bookCover: "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api",
-        bookShelf: "read"
-      },
-      {
-        bookTitle: "The Adventures of Tom Sawyer",
-        bookAuthors: "Mark Twain",
-        bookCover: "http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api",
-        bookShelf: "read"
       }
     ]
 
@@ -101,11 +65,11 @@ class BooksApp extends React.Component {
                     <h2 className="bookshelf-title">{shelf.title}</h2>
                     <div className="bookshelf-books">
                       <ol className="books-grid">
-                        {books.filter(book => book.bookShelf === shelf.category).map((book,index) => (
+                        {this.state.books.filter(book => book.shelf === shelf.category).map((book,index) => (
                           <li key={index}>
                             <div className="book">
                               <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.bookCover})` }}></div>
+                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                                 <div className="book-shelf-changer">
                                   <select>
                                     <option value="none" disabled>Move to...</option>
@@ -116,8 +80,10 @@ class BooksApp extends React.Component {
                                   </select>
                                 </div>
                               </div>
-                              <div className="book-title">{book.bookTitle}</div>
-                              <div className="book-authors">{book.bookAuthors}</div>
+                              <div className="book-title">{book.title}</div>
+                              {book.authors.map(author =>
+                                <div className="book-authors">{author}</div>
+                              )}
                             </div>
                           </li>
                         ))}
